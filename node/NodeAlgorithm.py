Changed to more convenient coordinate system. Fixed velocity calculation. Updated requirements. Added animation of position (animation works, but position caluclation is not ready).

node/NodeAlgorithm.py
@@ -1,16 +1,18 @@
-#region Import packages and functions
+# region Import packages and functions
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 
 from node.result import Result
-from node.rotation import rotate_to_earth_frame
+from node.rotation import rotate_to_earth_frame, change_coordinate_system
 from node.touchdown import *
 from node.velocity import *
 from node.position import *
+from node.orientation import *
 from node.animation import animate
 
-#endregion
+
+# endregion
 
 
 class NodeAlgorithm:
@@ -45,36 +47,52 @@ class NodeAlgorithm:
         :param df: sensor data (containing feature columns) as dataframe
         :return: result
         """
-        # Extract feature columns
-        acc_body = np.asarray(df[['accX[mg]', 'accY[mg]', 'accZ[mg]']], dtype=float) * 10 ** -3                 #[g]
-        gyro_body = np.asarray(df[['gyroX[mdps]', 'gyroY[mdps]', 'gyroZ[mdps]']], dtype=float) * 10 ** -3       #[dps]
-        mag_body = np.asarray(df[['magX[mG]', 'magY[mG]', 'magZ[mG]']], dtype=float) * 10 ** -3                 #[gauss]
+        # region Preparation
+        # Extract feature columns and change to new coordinate system, where z is up, y is out and x is forward
+        # when sensor is placed with USB in and screws out on right foot
+        acc_body = change_coordinate_system(
+            np.asarray(df[['accX[mg]', 'accY[mg]', 'accZ[mg]']], dtype=float) * 10 ** -3)  # [g]
+        gyro_body = change_coordinate_system(
+            np.asarray(df[['gyroX[mdps]', 'gyroY[mdps]', 'gyroZ[mdps]']], dtype=float) * 10 ** -3)  # [dps]
+        mag_body = change_coordinate_system(
+            np.asarray(df[['magX[mG]', 'magY[mG]', 'magZ[mG]']], dtype=float) * 10 ** -3)  # [gauss]
 
         # Rotate to earth frame
         self.acc_earth, self.gyro_earth, self.mag_earth = rotate_to_earth_frame(acc_body, gyro_body, mag_body,
-                                                                                self.freq, plot_rotation=True)
+                                                                                self.freq, plot_rotation=False)
         # Transform acc data to have SI units
         self.g_to_SI()
+        # endregion
 
-        # Find touchdowns
+        # region Find touchdowns
         touchdowns = find_touchdowns_gyro(self.gyro_earth, self.freq)
         touchdowns_acc = find_touchdowns_acc(self.acc_earth, self.freq)
 
         # Compare touchdowns
-        #compare_touchdowns(touchdowns, touchdowns_acc, self.acc_earth, self.gyro_earth, self.freq,
+        # compare_touchdowns(touchdowns, touchdowns_acc, self.acc_earth, self.gyro_earth, self.freq,
         #                   name1='gyro-touchdowns', name2='acc-touchdowns')
 
-        # Find velocity
-        velocity = find_velocity(self.acc_earth, touchdowns, self.freq, plot_drift=False, plot_vel=True)
-        velocity_cumtrapz = find_velocity_cumtrapz(self.acc_earth, self.freq, plot_vel=False)
+        # endregion
 
-        # Find position
-        position = find_position(velocity, touchdowns, self.freq, plot_drift=False)
-        animate(position)
-        position_cumtrapz = find_position_cumtrapz(velocity_cumtrapz, touchdowns, self.freq, plot_drift=True)
-        animate(position_cumtrapz)
-        np.savetxt(r"C:\\Users\\Hanne Maren\\Documents\\nivo\\python-experiments\\pos.csv", position, delimiter=",")
+        # region Find velocity
+        velocity_manual = find_velocity(self.acc_earth, touchdowns, self.freq, plot_drift=True, plot_vel=True)
+        velocity_cumtrapz = find_velocity_cumtrapz(self.acc_earth, self.freq, plot_vel=True)
+        # endregion
 
+        # region Find position
+        # First method: integrate velocity manually
+        position_manual = find_position(velocity_manual, touchdowns, self.freq, plot_drift=False)
+        # animate(position_manual)  # Animation does not look good. Christ
+
+        # Second method: integrate using cumtrapz. By using velocity_cumtrapz, position_cumtrapz increase rapidly,
+        # but by using velocity_manual result is basically the same as pos_man
+        position_cumtrapz = find_position_cumtrapz(velocity_cumtrapz, touchdowns, self.freq, plot_drift=False)
+        # animate(position_cumtrapz)  # Animation does not look good. Christ
+        # endregion
+
+        # region Find orientation
+        orientation = find_orientation_gyro(self.gyro_earth, self.freq)
+        # endregion
 
         return self.result
 
@@ -83,8 +101,8 @@ class NodeAlgorithm:
         Convert acceleration data to SI units and linear acceleration (remove gravity)
         :return:
         """
-        self.acc_earth *= 9.807     #[m/s/s]
-        self.acc_earth -= [9.087, 0, 0]
+        self.acc_earth *= 9.807  # [m/s/s]
+        self.acc_earth += [0, 0, 9.807]
 
     def plot(self):
         """
@@ -117,9 +135,3 @@ class NodeAlgorithm:
         ax3.legend(loc='upper right')
 
         plt.show()
-
-
-
-
-
-

node/rotation.py
@@ -60,7 +60,7 @@ def rotate_to_earth_frame(acc_body, gyro_body, mag_body, fs=100, plot_rotation=F
     :param plot_rotation: Plots acc before and after rotation
     :return: acc, gyro and mag in earth frame
     """
-    acc_earth_calib = [1, 0, 0]
+    acc_earth_calib = [0, 0, -1]
     acc_body_calib = acc_body[0, :]
 
     rot = rotation_matrix_from_vectors(acc_body_calib, acc_earth_calib)
@@ -82,91 +82,19 @@ def rotate_to_earth_frame(acc_body, gyro_body, mag_body, fs=100, plot_rotation=F
     return acc_earth, gyro_earth, mag_earth
 
 
-#region Shortcut to do what is done in NodeAlgorithm
-if __name__ == '__main__':
+def change_coordinate_system(array):
+    """
+    Change to new coordinate system, where z is up, y is out and x is forward
+    when sensor is placed with USB in and screws out on right foot
 
-    # Load df made in current_df.py
-    df = pd.read_pickle('current_df.pkl')
+    Old coordinate system is such that x is down, y is backwards and z is out
+    for the same sensor location
+    :param array: array with dim (len,3) in the old coordinate system
+    :return: new_array: array with dim(len,3) in the new coordinate system
+    """
+    new_array = array.copy()
+    new_array[:, 0] = -array[:, 1]      # -x --> x
+    new_array[:, 1] = array[:, 2]       # z --> y
+    new_array[:, 2] = -array[:, 0]      # -x --> z
 
-    acc = np.asarray(df[['accX[mg]', 'accY[mg]', 'accZ[mg]']], dtype=float)*10**-3
-    gyro = np.asarray(df[['gyroX[mdps]', 'gyroY[mdps]', 'gyroZ[mdps]']], dtype=float)*10**-3
-    mag = np.asarray(df[['magX[mG]', 'magY[mG]', 'magZ[mG]']], dtype=float)*10**-3
-
-    start = 0
-    stop = 2000
-    acc = acc[start:stop]
-    gyro = gyro[start:stop]
-    mag = mag[start:stop]
-
-    acc_earth = [1, 0, 0]
-    acc_body = acc[0, :]
-
-    rot = rotation_matrix_from_vectors(acc_body, acc_earth)
-
-    acc_check = acc.copy()
-    gyro_check = gyro.copy()
-    mag_check = mag.copy()
-
-    for i in range(len(acc[:, 0])):
-        acc[i, :] = rot.dot(acc[i, :])
-        gyro[i, :] = rot.dot(gyro[i, :])
-        mag[i, :] = rot.dot(mag[i, :])
-
-    #endregion
-
-    #region Plot
-    fig, ((ax1, ax2, ax3), (ax4, ax5, ax6),(ax7,ax8,ax9)) = plt.subplots(3,3)
-    ax1.set_title("AccX")
-    ax1.set_ylabel("g")
-    ax1.plot(acc_check[:, 0], 'r', label="Body frame")
-    ax1.plot(acc[:, 0], 'g', label="Earth frame")
-    ax1.legend(loc=1)
-
-    ax2.set_title("AccY")
-    ax2.set_ylabel("g")
-    ax2.plot(acc_check[:, 1], 'r', label="Body frame")
-    ax2.plot(acc[:, 1], 'g', label="Earth frame")
-    ax2.legend(loc=1)
-
-    ax3.set_title("AccZ")
-    ax3.set_ylabel("g")
-    ax3.plot(acc_check[:, 2], 'r', label="Body frame")
-    ax3.plot(acc[:, 2], 'g', label="Earth frame")
-    ax3.legend(loc=1)
-
-    ax4.set_title("GyroX")
-    ax4.set_ylabel("dps")
-    ax4.plot(gyro_check[:, 0], 'r', label="Body frame")
-    ax4.plot(gyro[:, 0], 'g', label="Earth frame")
-    ax4.legend(loc=1)
-
-    ax5.set_title("GyroY")
-    ax5.set_ylabel("dps")
-    ax5.plot(gyro_check[:, 1], 'r', label="Body frame")
-    ax5.plot(gyro[:, 1], 'g', label="Earth frame")
-    ax5.legend(loc=1)
-
-    ax6.set_title("GyroZ")
-    ax6.set_ylabel("dps")
-    ax6.plot(gyro_check[:, 2], 'r', label="Body frame")
-    ax6.plot(gyro[:, 2], 'g', label="Earth frame")
-    ax6.legend(loc=1)
-
-    ax7.set_title("MagX")
-    ax7.set_ylabel("gauss")
-    ax7.plot(mag_check[:, 0], 'r', label="Body frame")
-    ax7.plot(mag[:, 0], 'g', label="Earth frame")
-    ax7.legend(loc=1)
-
-    ax8.set_title("MagY")
-    ax8.set_ylabel("gauss")
-    ax8.plot(mag_check[:, 1], 'r', label="Body frame")
-    ax8.plot(mag[:, 1], 'g', label="Earth frame")
-    ax8.legend(loc=1)
-
-    ax9.set_title("MagZ")
-    ax9.set_ylabel("gauss")
-    ax9.plot(mag_check[:, 2], 'r', label="Body frame")
-    ax9.plot(mag[:, 2], 'g', label="Earth frame")
-    ax9.legend(loc=1)
-#endregion
+    return new_array

node/touchdown.py
@@ -196,7 +196,7 @@ def find_touchdowns_gyro(gyro, fs):
     lp_dgyro_abs = filterLP(order, cutOff, fs, dgyro_abs)
 
     threshold = 3
-    touchdowns = np.asarray(lp_dgyro_abs[:, 1] <= threshold, dtype=int)
+    touchdowns = np.asarray(lp_dgyro_abs[:, 0] <= threshold, dtype=int)
 
     return touchdowns
 
@@ -216,9 +216,7 @@ def find_touchdowns_acc(acc, fs):
     # OBS. Only valid for walk. Need different threshold for different gaits...
     # 1.3) Steps is where envelope of lowpass filtered accX is under a certain threshold
     threshold = 2.5
-    env = np.abs(signal.hilbert(accLP[:, 0]))
-    #envLP = filterLP(order, cutOff, fs, env)
-    #touchdowns = np.asarray(envLP <= threshold, dtype=int)
+    env = np.abs(signal.hilbert(accLP[:, 2]))
     touchdowns = np.asarray(env <= threshold, dtype=int)
 
     return touchdowns
@@ -267,41 +265,3 @@ def compare_touchdowns(method1, method2, acc, gyro, fs, name1='method 1', name2=
     return 0
 
 
-if __name__ == '__main__':
-    #region Shortcut to do what is done in NodeAlgrithm
-    # Load df made in current_df.py
-    df = pd.read_pickle('current_df.pkl')
-
-
-    acc = np.asarray(df[['accX[mg]', 'accY[mg]', 'accZ[mg]']], dtype=float)*10**-3
-    gyro = np.asarray(df[['gyroX[mdps]', 'gyroY[mdps]', 'gyroZ[mdps]']], dtype=float)*10**-3
-    mag = np.asarray(df[['magX[mG]', 'magY[mG]', 'magZ[mG]']], dtype=float)*10**-3
-
-    #region Plot raw data
-    start = 0
-    stop = 2000
-    acc, gyro, mag, time = plot_raw_data(start, stop, acc, gyro, mag)
-    #endregion
-
-    touchdowns = np.asarray(gyro[:, 2] > gyro[:, 0], dtype=int)
-
-    # Absolute value of gradient of gyro
-    dgyro_abs = np.abs(np.gradient(gyro, axis=0))
-
-
-    # Filter dgyro
-    order = 1
-    cutOff = 10
-    fs = 100
-    dgyro_abs_lp = filterLP(order, cutOff, fs, dgyro_abs)
-
-    threshold = 3
-    steps = np.asarray(dgyro_abs_lp[:, 1] <= threshold, dtype=int)
-
-    # Plot result
-    plt.figure()
-    plt.plot(steps)
-    plt.plot(dgyro_abs_lp)
-    plt.show()
-
-    #endregion

node/velocity.py
@@ -1,7 +1,92 @@
 import numpy as np
-from node.position import remove_drift
 from scipy.integrate import cumtrapz
 import matplotlib.pyplot as plt
+
+
+def remove_drift(array, touchdowns, fs, plot_drift=False, ylabel='Integrated value'):
+    """
+    Remove drift after integrating
+    :param array: Integrated array to remove drift from [m/s]
+    :param touchdowns: np.array with 0 where leg is in motion and 1 where leg is at the ground
+    :param fs: sampling frequency [Hz]
+    :param plot_drift: Plots effect of removing drift if true
+    :param ylabel: Label to y axis of plot. Default value is "Integrated value".
+    :return: vel: velocity array without drift [m/s]
+    """
+    # Set marker where motion start and stop
+    diff = np.diff(np.asarray(touchdowns, dtype=int))
+    start_motion = np.asarray(np.where(diff == -1)).flatten()  # Last index with zero value
+    stop_motion = np.asarray(np.where(diff == 1)).flatten()  # Last index with non-zero value
+
+    # Makes sure starting motion in the beginning, and stopping at the end
+    if stop_motion[0] < start_motion[0]:
+        start_motion = np.concatenate([[0], start_motion])
+
+    if stop_motion[-1] < start_motion[-1]:
+        stop_motion = np.concatenate([stop_motion, [len(array) - 1]])
+
+    array_check = array.copy()
+    # Remove drift
+    for i in range(0, len(stop_motion)):
+        # Find number of steps in the "motion"
+        samples = stop_motion[i] - start_motion[i]
+
+        # Find how much the drift is per sample vel[stop_motion[i]+1,:] == 0
+        stepwise_drift = array[stop_motion[i], :] / samples
+
+        # Find drift to remove from each step in the motion
+        drift = [stepwise_drift * i for i in range(1, samples + 1)]
+
+        # Remove drift
+        array[start_motion[i] + 1:stop_motion[i] + 1, :] = array[start_motion[i] + 1:stop_motion[i] + 1, :] - drift
+
+    # region Plot effect of removing drift
+    if plot_drift:
+        step_start = int((start_motion[5] + stop_motion[4]) / 2)
+        step_stop = stop_motion[5]
+
+        #fig, ((ax1, ax2, ax3), (ax4, ax5, ax6)) = plt.subplots(2, 3)
+        fig, (ax1, ax2, ax3) = plt.subplots(1, 3, sharex=True)
+        time = np.linspace(0, (step_stop-step_start) / fs, step_stop-step_start)
+
+        ax1.set_title("x direction")
+        ax1.plot(time, array_check[step_start:step_stop, 0], label="Before ")
+        ax1.plot(time, array[step_start:step_stop, 0], label="After ")
+        ax1.legend(loc='upper left')
+
+        ax2.set_title("y direction")
+        ax2.plot(time, array_check[step_start:step_stop, 1], label="Before")
+        ax2.plot(time, array[step_start:step_stop, 1], label="After ")
+        ax2.legend(loc='upper left')
+
+        ax3.set_title("z direction")
+        ax3.plot(time, array_check[step_start:step_stop, 2], label="Before ")
+        ax3.plot(time, array[step_start:step_stop, 2], label="After ")
+        ax3.legend(loc='upper left')
+        '''
+        ax4.set_title("Multiple steps, x direction")
+        ax4.plot(array_check[0:step_stop, 0], label="Before ")
+        ax4.plot(array[0:step_stop, 0], label="After ")
+        ax4.legend(loc='upper left')
+
+        ax5.set_title("Multiple steps, y direction")
+        ax5.plot(array_check[0:step_stop, 1], label="Before")
+        ax5.plot(array[0:step_stop, 1], label="After ")
+        ax5.legend(loc='upper left')
+
+        ax6.set_title("Multiple steps, z direction")
+        ax6.plot(array_check[0:step_stop, 2], label="Before ")
+        ax6.plot(array[0:step_stop, 2], label="After ")
+        ax6.legend(loc='upper left')
+        '''
+        fig.text(0.5, 0.04, 'Time [s]', ha='center')
+        fig.text(0.04, 0.5, ylabel, va='center', rotation='vertical')
+        plt.show()
+
+        plt.show()
+    # endregion
+
+    return array
 
 
 def plot_velocity(vel, fs):